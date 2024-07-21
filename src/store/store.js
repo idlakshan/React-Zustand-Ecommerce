df64fd677982ec7db16366b64b4841530f810465
idlakshan <id.lakshan21@gmail.com>
2024-07-21 22:22:16 +0530

    feat: update addToCart functionality

--- src/store/store.js
+++ src/store/store.js
@@ -6,9 +6,17 @@ const useStore = create((set) => ({
     addProduct: (product) => set((state) => ({
         products: [...state.products, product]
     })),
-    addToCart: (product) => set((state) => ({ 
-        cart: [...state.cart, product]
-     })),
+    addToCart: (product) => set((state) => {
+        const existingProduct = state.cart.find((p) => p.id === product.id);
+        if (existingProduct) {
+          return {
+            cart: state.cart.map((p) =>
+              p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
+            ),
+          };
+        }
+        return { cart: [...state.cart, { ...product, quantity: 1 }] };
+      }),
 
 }));
 
